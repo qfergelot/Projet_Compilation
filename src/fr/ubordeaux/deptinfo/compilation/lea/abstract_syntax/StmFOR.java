7d2e54848d0d9b3be9a855f22c63c63c08116562
Popof <jeanchristopheblin1@hotmail.fr>
2020-05-09 18:38:50 +0200

    FOR

--- src/fr/ubordeaux/deptinfo/compilation/lea/abstract_syntax/StmFOR.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/abstract_syntax/StmFOR.java
@@ -5,41 +5,60 @@ import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
 
 public class StmFOR extends StmTernary {
 
-	private Expr expr;
+	private Expr test;
 
-	public StmFOR(Expr expr, Stm stm1, Stm stm2, Stm stm3, int line, int column) {
+	public StmFOR(Expr test, Stm stm1, Stm stm2, Stm stm3, int line, int column) {
 		super(stm1, stm2, stm3, line, column);
-		this.expr = expr;
-	}
-	
-	@Override
-	public void checkType() throws TypeException {
-		expr.checkType();
-		if (this.getFirst() != null)
-			this.getFirst().checkType();
-		if (this.getSecond() != null)
-			this.getSecond().checkType();
-		if (this.getThird() != null)
-			this.getThird().checkType();
-		expr.getType().assertType(this, TypeCode.BOOLEAN);
+		this.test = test;
 	}
 
 	@Override
 	public String generateCode() throws CodeException {
 		String result = "";
 		result += super.generateCode();
-		// TODO
-		result += tab() + "_FOR_" + this.getId() + ":" + NL;
-		this.incIndent();
-		result += tab() + "// Code FOR ici..." + NL;
-		result += tab() + "printf(\"--- Manque FOR...\\n\");" + NL;
-		this.decIndent();
+
+		String var = "_for_test__" + this.getId();
+		String label_loop = "_for_label_loop__" + this.getId();
+		String label_end = "_for_label_end__" + this.getId();
+
+		result += tab() + "int " + var + ";" + NL;
+		result += tab() + getFirst().generateCode() + NL;
+		result += tab() + label_loop + ":{" + NL;
+		incIndent();
+			result += tab() + var + " = " + test.generateCode() + ";" + NL;
+			result += tab() + "if (!" + var + ")" + NL;
+			incIndent();
+				result += tab() + "goto " + label_end + ";" + NL;
+			decIndent();
+			result += tab() + "{" + NL;
+			incIndent();
+				result += getThird().generateCode();
+				result += getSecond().generateCode();
+			decIndent();
+			result += tab() + "}" + NL;
+			result += tab() + "goto " + label_loop + ";" + NL;
+		decIndent();
+		result += tab() + "}" + NL;
+		result += tab() + label_end + ":{}" + NL;
+
 		return result;
 	}
 
 	@Override
+	public void checkType() throws TypeException {
+		test.checkType();
+		if (this.getFirst() != null)
+			this.getFirst().checkType();
+		if (this.getSecond() != null)
+			this.getSecond().checkType();
+		if (this.getThird() != null)
+			this.getThird().checkType();
+		test.getType().assertType(this, TypeCode.BOOLEAN);
+	}
+
+	@Override
 	public String toString() {
-		return "for (... ; ... ; ...)";
+		return "for (... ; " + test + " ; ...)";
 	}
 
 }
